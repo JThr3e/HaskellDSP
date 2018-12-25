1a05f3a854cfcffcb026c104697b6d8062378562
Jthr3e <joseph.primmer@gmail.com>
2018-12-25 17:08:14 -0500

    implemented convolution using FFT/IFFT

--- fft/fft.hs
+++ fft/fft.hs
@@ -2,16 +2,21 @@ import Data.List.Split
 import Data.Bits
 
 main = do
-	putStrLn ("Input desired function: fft, ifft")
-	fun <- getLine
-	listNums <- readNumList
-	let len = (length listNums)
-	if (((.&.) len (len - 1)) == 0) --checks if list length is power of 2
-		then    --print (listNums)
-		if (fun == "fft") then print (fft listNums)
-		else if	(fun == "ifft") then print (ifft listNums)
-		else putStrLn ("invalid function")
-	else putStrLn ("List length must be a power of 2")
+	putStrLn ("Enter a real number sequence to convolve")
+	real1 <- readRealList
+	putStrLn ("Enter another real number sequence to convolve")
+	real2 <- readRealList
+	print (convolve real1 real2)
+	--putStrLn ("Input desired function: fft, ifft")
+	--fun <- getLine
+	--listNums <- readNumList
+	--let len = (length listNums)
+	--if (((.&.) len (len - 1)) == 0) --checks if list length is power of 2
+	--	then    --print (listNums)
+	--	if (fun == "fft") then print (fft listNums)
+	--	else if	(fun == "ifft") then print (ifft listNums)
+	--	else putStrLn ("invalid function")
+	--else putStrLn ("List length must be a power of 2")
 
 --Read in a string, turn into list, turn into tuples
 --Tuples here represent complex values (Re, Im)
@@ -24,6 +29,13 @@ readNumList = do
 	let imagList = splitOn "," imagFloat
 	return (zip (map toFloat realList) (map toFloat imagList))
 toFloat s = (read s :: Float)
+
+readRealList = do
+	putStrLn ("Input a list of real floats delimited by \",\"")
+        realFloat <- getLine
+	let realList = splitOn "," realFloat
+ 	return  (map toRealTuple realList)
+toRealTuple s = ((read s :: Float), 0)
 
 --Duplicate the elements in a list and append to the end
 duplicate :: [(Float, Float)] -> [(Float, Float)]
@@ -61,7 +73,8 @@ roundN n = \(a,b) -> ((fromInteger $ round $ a * (10^n)) / (10.0^^n),
 		     (fromInteger $ round $ b * (10^n)) / (10.0^^n))
 
 --Find next nearest power of 2
-nextPow2 x = 2^(ceiling(logBase 2 x))
+nextPow2 :: Int -> Int
+nextPow2 x = 2^(ceiling(logBase 2.0 (fromIntegral x::Float)))
 
 --Create a list of n zeros
 listOfNZeros n = take n (repeat (0.0,0.0))
@@ -77,4 +90,17 @@ ifft :: [(Float, Float)] -> [(Float, Float)]
 ifft [] = []
 ifft (x:xs) = map (roundN 3)(map (divN (fromIntegral (length (x:xs))::Float))
 		  (fft (x : (reverse xs))))
-   
+
+--Zeropad n number of zero pairs to list x
+zeropad n x = x ++ (listOfNZeros n)
+
+--Map but with 2 args
+map2 :: ((Float, Float) -> (Float, Float) -> (Float, Float)) -> [(Float, Float)] -> [(Float, Float)] -> [(Float, Float)]
+map2 x [] [] = []
+map2 x (a:as) (b:bs) = x a b : map2 x as bs 
+
+--Convolution of 2 Real Signals in the frequency domain
+convolve :: [(Float, Float)] -> [(Float, Float)] -> [(Float, Float)] 
+convolve [][] = [] 
+convolve x y = ifft (map2 complexMul (fft(zeropad (nextPow2((length x)+(length y)-1)-(length x)) x)) (fft( zeropad (nextPow2((length x)+(length y)-1)-(length y)) y)))
+ 
